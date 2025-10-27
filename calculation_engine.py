d7532a3033e3f577d15f7212cea23d15e4fe4131
Hukseflux - Matthijs ATES <matthijs.ates@hukseflux.com>
2025-10-27 16:59:40 +0100

    Implemented new binning limit calculation for temporal resolution increase, including a smuggling limit

--- calculation_engine.py
+++ calculation_engine.py
@@ -1,6 +1,8 @@
 from my_dataclasses import Variable
 import datetime
 import numpy as np
+from datetime import timedelta
+
 
     
 class CalculationEngine:
@@ -66,17 +68,48 @@ class CalculationEngine:
         print(new_timestep)
         
                 
-    def increaseTemporalResolution(self, var, new_timestep, benchmark_time):
+    def increaseTemporalResolution(self, var, new_timestep, benchmark_time, smuggle_limit=0):
         """ Returns array of values for the new time resolution for a given variable, benchmark time is a specified border between 2 bins """
+        #Check if temporal operations make sense for this variable (e.g. not a float)
+        if isinstance(var.values, float):
+            raise ValueError(f"Rebinning of variable {var.name} terminated, variable is a constant.")
+        
         #Convert new timestep to datetime object
         new_timestep_datetime = datetime.timedelta(seconds=new_timestep)
         #identify index of the benchmark time ###!!!
-        print("WARNING: benchamrk times can be inside a timestep ; i.e. a timestep can span 11:55-12:05 and benchmark time is 12:00 - what then?")
+        print("WARNING: benchmark times can be inside a timestep ; i.e. a timestep can span 11:55-12:05 and benchmark time is 12:00 - handle correct time split/aggregation here")
         
         #calculate increase factor
-        factor = benchmark_time / var.timestep
+        factor = new_timestep / var.timestep
+        print(var.start_time)
+        print(var.first_time)
         #calculate from where we should start binning:
+        present_bin_edges = np.linspace(var.start_time, var.end_time, len(var.values)+1)
+        print(present_bin_edges)
         
+        #We calculate the new start and end times by seeing where bin limits - given the benchmark - fit in the previous timerange
+        #Note: we allow to smuggle a a specified amount; to allow manual avoiding of instances where an hour of data is discarded based on a small time mismatch.
+        delta_start = (var.start_time - benchmark_time).total_seconds()
+        offset_steps = np.floor(delta_start/new_timestep)
+        first_edge = benchmark_time + timedelta(seconds=offset_steps * new_timestep)
+        if first_edge<var.start_time:
+            if (var.start_time - first_edge).total_seconds() > smuggle_limit:
+                first_edge += timedelta(seconds=new_timestep)
+                
+        
+        delta_end = (var.end_time - benchmark_time).total_seconds()
+        offset_steps_end = np.ceil(delta_end / new_timestep)
+        last_edge = benchmark_time + timedelta(seconds=offset_steps_end * new_timestep)
+        if last_edge>var.end_time:
+            if (last_edge - var.end_time).total_seconds() > smuggle_limit:
+                last_edge -= timedelta(seconds=new_timestep)
+        
+        
+        print(var.start_time)
+        print(var.end_time)
+        print(first_edge)
+        print(last_edge)
+        #factor = benchmark_time / var.timestep
         return
         
     
