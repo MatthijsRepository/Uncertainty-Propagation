76f35f9f22177e26202f6bd57eba3bad0dacf0c1
Hukseflux - Matthijs ATES <matthijs.ates@hukseflux.com>
2025-11-19 21:55:42 +0100

    Added wrapper functions for variable evaluations, uncertainty calculations

--- job_handler.py
+++ job_handler.py
@@ -18,24 +18,31 @@ class JobHandler:
         self.time_engine = None
         
         self.variables = None
+        self.derived_variables_names = None
         self.var_csv_pointers = None
+        
+        ##computational control flow booleans
+        self.basic_variables_validated = False
+        self.csv_variables_populated   = False
         return
     
-    def readEquationTree(self, filepath):
+    def LoadEquationTree(self, filepath):
         self.equation_tree_reader = EquationTreeReader()
         self.variables, self.var_csv_pointers = self.equation_tree_reader.parse(filepath)
         del self.equation_tree_reader
         
-    def initializeEquationTree(self):
-        if self.variables is None:
-            raise ValueError("Cannot initialize equation tree: variable registry is empty.")
+        if len(self.var_csv_pointers) == 0:
+            self.csv_variables_populated = True
         
         self.equation_engine = EquationEngine(self.variables)
+        self.derived_variables_names = self.equation_engine.derived_variables
+        
         self.equation_engine.checkEquationTreeConsistency(silent=True)
         self.equation_engine.populateEquationTreeDependencies()
         self.equation_engine.populateEquationTreeTimeSumSettings()
         self.equation_engine.buildEquationTreeExecutables()
         
+
     
     def prepareEngines(self):
         self.time_engine = TimeEngine()
@@ -52,9 +59,13 @@ class JobHandler:
         for var in self.variables.values():
             if not var.is_hardcoded:
                 var.reset()
+        
+        self.basic_variables_validated = False
+        if len(self.var_csv_pointers) != 0:
+            self.csv_variables_populated   = False
       
     
-    def populateVariablesFromCSV(self, filepaths, metadata, reset_registry=True):
+    def readFromCSV(self, filepaths, metadata, reset_registry=True):
         if reset_registry:
             self.resetVariableRegistry()
         
@@ -82,7 +93,52 @@ class JobHandler:
                     break
             if not found:
                 raise ValueError(f"CSV data does not contain data named {column_name}.")
+        self.csv_variables_populated = True
     
+    def validateBasicVariables(self):
+        if self.variables is None:
+            raise ValueError("Validation of basic variables failed: no existing variable registry found.")
+        if not self.csv_variables_populated:
+            print("WARNING: trying to perform calculations while no CSV data appears to be loaded. Crash may occur.")
+            
+        self.calculation_engine.validateBasicVariables(equation_engine=self.equation_engine, variables=self.variables)
+        self.basic_variables_validated = True
+        return
+    
+    def evaluateVariable(self, var_name):
+        if not self.basic_variables_validated:
+            self.validateBasicVariables()
+        
+        var = self.variables.get(var_name)
+        if var is None:
+            raise ValueError(f"Tried to evaluate non-existing variable '{var_name}'.")
+            
+        self.calculation_engine.evaluateVariable(var)
+        
+    def evaluateAllVariables(self):
+        if not self.basic_variables_validated:
+            self.validateBasicVariables()
+        
+        for var_name in self.derived_variables_names:
+            self.calculation_engine.evaluateVariable(self.variables[var_name])
+            
+    def prepareAllDirectUncertainties(self):
+        self.uncertainty_engine.prepareAllDirectUncertainties()
+    
+    def prepareDownTreeDirectUncertainties(self, var_name):
+        var = self.variables.get(var_name)
+        if var is None:
+            raise ValueError(f"Tried to evaluate uncertainty for non-existing variable '{var_name}'.")
+        self.uncertainty_engine.prepareDownTreeDirectUncertainties(var)
+    
+    def calculateTotalUncertainty(self, var_name):
+        var = self.variables.get(var_name)
+        if var is None:
+            raise ValueError(f"Tried to evaluate uncertainty for non-existing variable '{var_name}'.")
+        self.uncertainty_engine.calculateTotalUncertainty(self, var_name)
+        
+        
+
     
 
 
@@ -98,16 +154,16 @@ if __name__=="__main__":
     filepaths = ["C:\\Users\\mate\\Desktop\\python\\Experimental\\testdata.csv", "C:\\Users\\mate\\Desktop\\python\\Experimental\\testdata.csv"]
     
     
-    job_handler = JobHandler()
+    job = JobHandler()
     
     
-    job_handler.readEquationTree(inputfile)
-    job_handler.initializeEquationTree()
+    job.LoadEquationTree(inputfile)
+
     
-    job_handler.resetVariableRegistry()
+    job.resetVariableRegistry()
         
     
-    job_handler.populateVariablesFromCSV(filepaths, metadata)
+    job.readFromCSV(filepaths, metadata)
     
     
     
